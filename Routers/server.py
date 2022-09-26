5ed1fe9e3d9ba5b1e55f9a21edc0898cbf3b8874
Javier Núñez <javijnb@gmail.com>
2022-09-26 16:58:23 +0200

    feat: added login and purchase endpoints

--- Routers/server.py
+++ Routers/server.py
@@ -1,13 +1,36 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import jwt
+import os
 
 from Models.AuthenticateRequestModel import AuthenticateRequestModel
 from Models.AuthenticateResponseModel import AuthenticateResponseModel
+from Models.PurchaseRequestModel import PurchaseRequestModel
+from Models.PurchaseResponseModel import PurchaseResponseModel
 
 app = FastAPI()
+load_dotenv()
+PORT = os.getenv('PORT')
+print(PORT)
 
-@app.get("/")
-async def main():
-    return {"message": "Hello World"}
+@app.get("/purchase")
+async def main(request: PurchaseRequestModel) -> PurchaseResponseModel:
+    concert = request.concert
+    number_of_tickets = request.number_of_tickets
+    transaction_id = request.transaction_id
+    token = request.token
+
+    try:
+        jwt.decode(token)
+    except Exception:
+        return {
+            "message": "Provided token was not valid", 
+            "success": "False"
+        }
+
+    response:PurchaseResponseModel = PurchaseResponseModel(ticket_url="url", transaction_id="12345")
+    return response
+
 
 @app.post("/authenticate")
 async def authenticate(request: AuthenticateRequestModel) -> AuthenticateResponseModel:
