26b283d4f35f938abf8e88c21509208c7d77d588
Javier Núñez <javijnb@gmail.com>
2022-09-26 20:44:57 +0200

    feat: added the logic for capacity updates and max capacity retriever

--- Infrastructure/CapacityChecker.py
+++ Infrastructure/CapacityChecker.py
@@ -1,20 +1,48 @@
-class CapacityChecker():
+from fastapi import HTTPException
+
+from Infrastructure.SQSReader import SQSReader
+from Infrastructure.SQSWriter import SQSWriter
+
+class CapacityChecker:
+
+    def __init__(self, capacity_queue_url:str):
+        self.capacity_queue_url = capacity_queue_url
 
     def update_capacity(self, event: str, number_of_tickets: int) -> bool:
 
-        # Si es posible, incrementa la capacidad del evento indicado en el número de tickets pasado por argumentos
-        current_capacity = self.get_current_capacity(event=event)
+        # Get current data in Capacity SQS Queue
+        sqs_reader = SQSReader()
+        message = sqs_reader.read_message(queue_url=self.capacity_queue_url)
+
+        # Read capacity from JSON message
+        for concert in message:
+            if concert["event"] == event:
+                current_capacity = concert["current_capacity"]
+                break
+
+        # Get max capacity for the requested event
         max_capacity = self.get_max_capacity(event=event)
 
+        # CHeck if possible to purchase the amount of requested tickets
         if (number_of_tickets + current_capacity) > max_capacity:
             return False
+
+        # Update the capacity
+        for concert in message:
+            if concert["event"] == event:
+                concert["current_capacity"] += number_of_tickets
+
+        # Send updated message with SQSWriter
+        sqs_writer = SQSWriter()
+        success = sqs_writer.write(queue_url=self.capacity_queue_url, message_body=message)
+        if not success:
+            raise HTTPException(status_code=400, detail="Unable to send message "+message+" to the SQS queue <"+self.capacity_queue_url+">")
+
+        if current_capacity == None:
+            raise HTTPException(status_code=400, detail="Unable to retrieve current capacity for requested event <"+event+">")
         
-        # Update capacity with SQSWriter
+        return current_capacity
 
-    def get_current_capacity(self, event:str) -> int:
-
-        # Get current capacity of the requested event with SQSReader
-        print()
 
     def get_max_capacity(self, event:str) -> int:
 
