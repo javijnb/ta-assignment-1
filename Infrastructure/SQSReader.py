79732b9239be83b82d30e289c910927a5ef3da02
Javier Núñez <javijnb@gmail.com>
2022-09-26 20:44:17 +0200

    feat: updated SQSreader and SQSwriter

--- Infrastructure/SQSReader.py
+++ Infrastructure/SQSReader.py
@@ -2,28 +2,30 @@ import boto3
 
 sqs = boto3.client("sqs")
 
-queue_url = 'SQS_QUEUE_URL'
+class SQSReader:
 
-# Leer mensaje de la cola
-response = sqs.receive_message(
-    QueueUrl=queue_url,
-    AttributeNames=[
-        'SentTimestamp'
-    ],
-    MaxNumberOfMessages=1,
-    MessageAttributeNames=[
-        'All'
-    ],
-    VisibilityTimeout=0,
-    WaitTimeSeconds=0
-)
+    def read_message(queue_url:str) -> str:
 
-message = response['Messages'][0]
-receipt_handle = message['ReceiptHandle']
+        # Leer mensaje de la cola especificada
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            AttributeNames=[
+                'SentTimestamp'
+            ],
+            MaxNumberOfMessages=1,
+            MessageAttributeNames=[
+                'All'
+            ],
+            VisibilityTimeout=0,
+            WaitTimeSeconds=0
+        )
 
-# Delete received message from queue
-sqs.delete_message(
-    QueueUrl=queue_url,
-    ReceiptHandle=receipt_handle
-)
-print('Received and deleted message: %s' % message)
+        message = response['Messages'][0]['Body']
+
+        # Para eliminar el mensaje, obtener el campo 'ReceiptHandle'
+        receipt_handle = message['ReceiptHandle']
+        sqs.delete_message(
+            QueueUrl=queue_url,
+            ReceiptHandle=receipt_handle
+        )
+        return message
