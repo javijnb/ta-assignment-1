1074238523818774a8509779adda8b699af80852
Javier Núñez <javijnb@gmail.com>
2022-09-29 19:35:03 +0200

    feat: email decoding from JWT and PDF manager call added to purchase operation

--- Routers/server.py
+++ Routers/server.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import jwt
 import os
 from Infrastructure.CapacityChecker import CapacityChecker
+from Infrastructure.PDFManager import PDFManager
 from Infrastructure.TokenBuilder import TokenBuilder
 from Infrastructure.UUIDGenerator import UUIDGenerator
 
@@ -18,6 +19,9 @@ load_dotenv()
 PORT = os.getenv('PORT')
 SECRET = os.getenv('SECRET')
 CAPACITY_QUEUE_URL = os.getenv('CAPACITY_QUEUE_URL')
+USERS_TABLE_NAME = os.getenv('USERS_TABLE_NAME')
+TICKETS_TABLE_NAME = os.getenv('TICKETS_TABLE_NAME')
+EVENTS_TABLE_NAME = os.getenv('EVENTS_TABLE_NAME')
 
 @app.get("/purchase")
 async def main(request: PurchaseRequestModel) -> PurchaseResponseModel:
@@ -27,7 +31,9 @@ async def main(request: PurchaseRequestModel) -> PurchaseResponseModel:
     token = request.token
 
     try:
-        jwt.decode(token, SECRET, algorithms=["HS256"])
+        decoded_jwt = jwt.decode(token, SECRET, algorithms=["HS256"])
+        email = decoded_jwt["email"]
+
     except Exception as e:
         return {
             "message": e, 
@@ -41,12 +47,19 @@ async def main(request: PurchaseRequestModel) -> PurchaseResponseModel:
         raise HTTPException(status_code=400, detail="Requested event is full. There are no more tickets on sale")
 
     # Guardar el ticket
-    # -> Error? Devolver mensaje de error
+    pdf_manager = PDFManager()
+    try:
+        ticket_url = pdf_manager.build_and_save_pdf(concert=requested_concert, number_of_tickets=requested_number_of_tickets, transaction_id=transaction_id, email=email)
+    except Exception as e:
+        return {
+            "message": e, 
+            "success": "False"
+        }
+
     # raise HTTPException(status_code=500, detail="Could not store ticket in the AWS Repository")
 
-
     # Enviar la URL en la respuesta
-    response:PurchaseResponseModel = PurchaseResponseModel(ticket_url="url", transaction_id=transaction_id)
+    response:PurchaseResponseModel = PurchaseResponseModel(ticket_url=ticket_url, transaction_id=transaction_id)
     return response
 
 
@@ -62,7 +75,7 @@ async def authenticate(request: AuthenticateRequestModel) -> AuthenticateRespons
 
     # Si éxito, devolver token de sesión
     if True:
-        new_token:str = TokenBuilder.new_token(SECRET)
+        new_token:str = TokenBuilder.new_token(email=email, secret=SECRET)
         response:AuthenticateResponseModel = AuthenticateResponseModel(token=new_token, success=True)
         return response
 
