23d509d3c95239b58e913a313196065f78d2788c
Javier Núñez <javijnb@gmail.com>
2022-10-18 21:16:52 +0200

    feat: added aws credentials parameters when created AWS classes in Infraestructure python classes

--- Infrastructure/CapacityChecker.py
+++ Infrastructure/CapacityChecker.py
@@ -6,14 +6,17 @@ from AWS.DynamoDBConnector import DynamoDBConnector
 
 class CapacityChecker:
 
-    def __init__(self, capacity_queue_url:str, events_dynamodb_table_name:str):
+    def __init__(self, capacity_queue_url:str, events_dynamodb_table_name:str, aws_key:str, aws_secret_key:str, aws_region:str):
         self.capacity_queue_url = capacity_queue_url
         self.events_dynamodb_table_name = events_dynamodb_table_name
+        self.aws_key = aws_key
+        self.aws_secret_key = aws_secret_key
+        self.aws_region = aws_region
 
     def update_capacity(self, event: str, number_of_tickets: int) -> bool:
 
         # Get current data in Capacity SQS Queue
-        sqs_reader = SQSReader()
+        sqs_reader = SQSReader(self.aws_key, self.aws_secret_key, self.aws_region)
         message = sqs_reader.read_message(queue_url=self.capacity_queue_url)
 
         # Read capacity from JSON message
@@ -35,7 +38,7 @@ class CapacityChecker:
                 concert["current_capacity"] += number_of_tickets
 
         # Send updated message with SQSWriter
-        sqs_writer = SQSWriter()
+        sqs_writer = SQSWriter(self.aws_key, self.aws_secret_key, self.aws_region)
         success = sqs_writer.write_message(queue_url=self.capacity_queue_url, message_body=message)
         if not success:
             raise HTTPException(status_code=400, detail="Unable to send message "+message+" to the SQS queue <"+self.capacity_queue_url+">")
@@ -49,7 +52,7 @@ class CapacityChecker:
     def get_max_capacity(self, event:str) -> int:
 
         # Get from DynamoDB repository max capacity for the requested event with SQSReader
-        dynamodb_connector = DynamoDBConnector()
+        dynamodb_connector = DynamoDBConnector(self.aws_key, self.aws_secret_key, self.aws_region)
         max_capacity = dynamodb_connector.get_item(
             table_name= self.events_dynamodb_table_name,
             key="event", 
--- Infrastructure/PDFManager.py
+++ Infrastructure/PDFManager.py
@@ -1,9 +1,14 @@
 from fpdf import FPDF
-import base64
+from AWS.S3Connector import S3Connector
+
+pdfs_folder = "../PDFs/"
 
 class PDFManager:
-    def __init__(self):
-        pass
+    def __init__(self, aws_key:str, aws_secret_key:str, aws_region:str, bucket_name:str):
+        self.aws_key = aws_key
+        self.aws_secret_key = aws_secret_key
+        self.aws_region = aws_region
+        self.bucket_name = bucket_name
 
     def build_and_save_pdf(self, concert:str, number_of_tickets:int, transaction_id:str, email:str) -> str:
         
@@ -11,20 +16,17 @@ class PDFManager:
         pdf = FPDF()
         pdf.add_page()
         pdf.set_font('Courier','B',16)
-        pdf.cell(40,10,'Concierto: '+concert)
-        pdf.cell(40,10,'Número de entradas: '+number_of_tickets)
-        pdf.cell(40,10,'Correo electrónico: '+email)
-        pdf.cell(40,10,'ID de la transacción de compra: '+transaction_id)
+        pdf.cell(40,10,'Concierto: '+concert, ln=1)
+        pdf.cell(40,10,'Número de entradas: '+number_of_tickets, ln=2)
+        pdf.cell(40,10,'Correo electrónico: '+email, ln=3)
+        pdf.cell(40,10,'ID de la transacción de compra: '+transaction_id, ln=4)
 
-        # Get PDF in base64 to be stored in DynamoDB
-        pdf_bytes = pdf.output(transaction_id+'.pdf','S').encode('UTF-8')
-        pdf_bytes_b64 = base64.b64encode(pdf_bytes)
-        pdf_b64 = pdf_bytes_b64.decode('UTF-8')
-        # TODO: this base64 returns a blank PDF, fix it
-        #print(pdf_b64)
+        # Create PDF
+        filename = transaction_id+'.pdf'
+        filepath = pdfs_folder + filename
+        pdf.output(filepath,'F')
 
-        # Save PDF in DynamoDB
-        url = "string"
-
-        # Return URL
+        # Save PDF in S3
+        s3_connector = S3Connector(self.aws_key, self.aws_secret_key, self.aws_region)
+        url = s3_connector.save_new_item(self.bucket_name, filename)
         return url
