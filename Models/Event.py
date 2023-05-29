c1c06b3c077282158bd6ae65ebd9f8cb43c55486
Javi Nunez <javijnb@gmail.com>
2023-05-29 17:13:52 +0200

    feat: added new field to Event model class

--- Models/Event.py
+++ Models/Event.py
@@ -1,4 +1,5 @@
 class Event:
-    def __init__(self, event_name:str, max_capacity: int):
+    def __init__(self, event_name:str, max_capacity: int, current_capacity: int = 0):
         self.event_name = event_name
         self.max_capacity = max_capacity
+        self.current_capacity = current_capacity
