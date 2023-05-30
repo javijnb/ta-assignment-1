a85c9e4de70bc9ee5ecc807c8eb1b3ea3c6bcea1
Javi Nunez <javijnb@gmail.com>
2023-05-30 13:46:02 +0200

    feat: added control of login and prepared for purchase routing

--- Frontend/main.py
+++ Frontend/main.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
-  
-# creates a Flask application
+import requests
+import json
+
+backend_url = "http://localhost:8000/authenticate"
+TOKEN = ""
+
 app = Flask(__name__)
-  
-  
-@app.route("/")
-def hello():
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        payload = {'email': email, 'password': password}
+        response = requests.post(backend_url, json=payload)
+        
+        if response.status_code == 200:
+            response_json = json.loads(response.text)
+            if response_json["success"] == True:
+                TOKEN = response_json["token"]
+                return "Página de compra"
+            
+        return 'Error en el inicio de sesión'
+
     return render_template('index.html')
-  
-# run the application
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(debug=True)
