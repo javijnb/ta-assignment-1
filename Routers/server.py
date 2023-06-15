a3e4769b851961d04db7069aed635610929d53c3
Javier Núñez <javijnb@gmail.com>
2023-06-15 12:59:48 +0200

    fix: updated software of EC2 backend

--- Routers/server.py
+++ Routers/server.py
@@ -9,11 +9,6 @@ from Infrastructure.PDFManager import PDFManager
 from Infrastructure.TokenBuilder import TokenBuilder
 from Infrastructure.UserAuthenticator import UserAuthenticator
 
-from Models.AuthenticateRequestModel import AuthenticateRequestModel
-from Models.AuthenticateResponseModel import AuthenticateResponseModel
-from Models.PurchaseRequestModel import PurchaseRequestModel
-from Models.PurchaseResponseModel import PurchaseResponseModel
-
 app = FastAPI()
 load_dotenv()
 
@@ -28,7 +23,6 @@ USERS_TABLE_NAME = os.getenv('USERS_TABLE_NAME')
 EVENTS_TABLE_NAME = os.getenv('EVENTS_TABLE_NAME')
 S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 
-
 # AWS Clients
 SQS_CLIENT = boto3.client('sqs', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_ACCESS_SECRET_KEY, region_name=AWS_REGION_NAME, aws_session_token=AWS_SESSION_TOKEN)
 SQS_REQUEST_QUEUE_NAME=os.getenv('SQS_REQUEST_QUEUE_NAME')
@@ -36,74 +30,117 @@ SQS_RESPONSE_QUEUE_NAME=os.getenv('SQS_RESPONSE_QUEUE_NAME')
 SQS_REQUEST_QUEUE_URL=os.getenv('SQS_REQUEST_QUEUE_URL')
 SQS_RESPONSE_QUEUE_URL=os.getenv('SQS_RESPONSE_QUEUE_URL')
 
-@app.get("/purchase")
-async def main(request: PurchaseRequestModel) -> PurchaseResponseModel:
+SQS_REQUEST_QUEUE_URL = SQS_REQUEST_QUEUE_URL + SQS_REQUEST_QUEUE_NAME
+SQS_RESPONSE_QUEUE_URL= SQS_RESPONSE_QUEUE_URL+ SQS_RESPONSE_QUEUE_NAME
 
-    # Recogemos información de la request
-    print("<PURCHASE> New request")
-    requested_concert = request.concert
-    requested_number_of_tickets = request.number_of_tickets
-    transaction_id = request.transaction_id
-    token = request.token
+while True:
 
-    try:
-        decoded_jwt = jwt.decode(token, SECRET, algorithms=["HS256"])
-        email = decoded_jwt["email"]
+    fetched_request = SQS_CLIENT.receive_message(QueueUrl=SQS_REQUEST_QUEUE_URL, MessageAttributeNames=['All'], MaxNumberOfMessages=1)
+    messages = list(fetched_request.keys())[0]
+    print(messages)
 
-    except Exception as e:
-        response:PurchaseResponseModel = PurchaseResponseModel(ticket_url="", transaction_id="", success=False, message="Purchase failed - Provided token was not valid")
-        return response
+    if messages == 'Messages':
 
-    # Actualizar capacidad en caso de ser posible
-    try:
-        capacity_checker = CapacityChecker(EVENTS_TABLE_NAME, AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY, AWS_REGION_NAME)
-        capacity_operation_success = capacity_checker.update_capacity(event=requested_concert, number_of_tickets=requested_number_of_tickets)
-        
-        if not capacity_operation_success:
-            response:PurchaseResponseModel = PurchaseResponseModel(ticket_url="", transaction_id="", success=False, message="Purchase failed - Requested event is full. There are no more tickets on sale")
-            return response
+        print("<NEW REQ> Mensaje recibido")
+        req_receipt_handle = fetched_request['Messages'][0]['ReceiptHandle']
+        SQS_CLIENT.delete_message(QueueUrl=SQS_REQUEST_QUEUE_URL, ReceiptHandle=req_receipt_handle)
+        print("<NEW REQ> Mensaje eliminado")
 
-        # Guardar el ticket
-        # pdf_manager = PDFManager(AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY, AWS_REGION_NAME, S3_BUCKET_NAME)
-        # ticket_url = pdf_manager.build_and_save_pdf(concert=requested_concert, number_of_tickets=requested_number_of_tickets, transaction_id=transaction_id, email=email)
-        ticket_url = "ticket mocked"
+        attributes = fetched_request['Messages'][0]['MessageAttributes']
+        jwt_token = attributes['jwt']['StringValue']
+        email = attributes['email']['StringValue']
+        transaction_id = attributes['transaction_id']['StringValue']
+        requested_event = attributes['event']['StringValue']
+        requested_tickets = attributes['tickets']['StringValue']
 
-    except Exception as e:
-        response:PurchaseResponseModel = PurchaseResponseModel(ticket_url="", transaction_id="", success=False, message="Purchase failed - "+str(e))
-        return response
+        try:
+            print("<PURCHASE> Handling ticket purchase...")
+            decoded_jwt = jwt.decode(jwt_token, SECRET, algorithms=["HS256"])
 
-    # Enviar la URL en la respuesta
-    response:PurchaseResponseModel = PurchaseResponseModel(ticket_url=ticket_url, transaction_id=transaction_id, success=True, message="Tickets purchased correctly")
-    return response
+            # Actualizar capacidad en caso de ser posible
+            capacity_checker = CapacityChecker(EVENTS_TABLE_NAME, AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY, AWS_REGION_NAME)
+            capacity_operation_success = capacity_checker.update_capacity(event=requested_event, number_of_tickets=requested_tickets)
+            
+            if not capacity_operation_success:
+                print("<PURCHASE ERROR> Cannot purchase - Event "+requested_event+" is full")
+                SQS_CLIENT.send_message(QueueUrl=SQS_RESPONSE_QUEUE_URL, MessageBody="error", MessageGroupId="TA", MessageAttributes={
+                    'email': {
+                        'StringValue': email,
+                        'DataType': 'String'
+                    },
+                    'requested_tickets': {
+                        'StringValue': requested_tickets,
+                        'DataType': 'String'
+                    },
+                    'requested_event': {
+                        'StringValue': requested_event,
+                        'DataType': 'String'
+                    },
+                    'transaction_id': {
+                        'StringValue': transaction_id,
+                        'DataType': 'String'
+                    },
+                    'error': {
+                        'StringValue': 'Purchase failed - Requested event is full. There are no more tickets on sale',
+                        'DataType': 'String'
+                    }
+                })
 
-    #TODO: devolver respuesta a la cola SQS
+            # Guardar el ticket
+            pdf_manager = PDFManager(AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY, AWS_REGION_NAME, S3_BUCKET_NAME)
+            ticket_url = pdf_manager.build_and_save_pdf(concert=requested_event, number_of_tickets=requested_tickets, transaction_id=transaction_id, email=email)
+            ticket_url = "ticket url mocked"
 
+            # Enviar la URL en la respuesta
+            print("<PURCHASE SUCCESS> Purchase of "+requested_tickets+" for event "+requested_event+" was successful")
+            SQS_CLIENT.send_message(QueueUrl=SQS_RESPONSE_QUEUE_URL, MessageBody="success", MessageGroupId="TA", MessageAttributes={
+                'email': {
+                    'StringValue': email,
+                    'DataType': 'String'
+                },
+                'requested_tickets': {
+                    'StringValue': requested_tickets,
+                    'DataType': 'String'
+                },
+                'requested_event': {
+                    'StringValue': requested_event,
+                    'DataType': 'String'
+                },
+                'transaction_id': {
+                    'StringValue': transaction_id,
+                    'DataType': 'String'
+                },
+                'ticket_url': {
+                    'StringValue': ticket_url,
+                    'DataType': 'String'
+                },
+                'message': {
+                    'StringValue': 'Tickets purchased correctly',
+                    'DataType': 'String'
+                }
+            })
 
-@app.post("/authenticate")
-async def authenticate(request: AuthenticateRequestModel) -> AuthenticateResponseModel:
-    
-    # Recoger parámetros
-    print("<AUTH> New request")
-    email = request.email
-    password = request.password
-
-    # Consulto par (email, pwd) en el repositorio (DynamoDB)
-    user_authenticator = UserAuthenticator(USERS_TABLE_NAME, AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY, AWS_REGION_NAME)
-    auth_success = user_authenticator.authenticate_user(email, password)
-    auth_success = True
-
-    # Si éxito, devolver token de sesión
-    if auth_success:
-        new_token:str = TokenBuilder.new_token(email=email, secret=SECRET)
-        response:AuthenticateResponseModel = AuthenticateResponseModel(token=new_token, success=True)
-        return response
-
-    response:AuthenticateResponseModel = AuthenticateResponseModel(token="", success=False)
-    return response
-
-
-@app.post("/main")
-async def rutina_main():
-    while True:
-        fetched_request = SQS_CLIENT.receive_message(QueueUrl=SQS_REQUEST_QUEUE_URL, MessageAttributeNames=['All'], MaxNumberOfMessages=1)
-        messages = list()
+        except Exception as e:
+            print("<ERROR> Unhandled error: ", e)
+            SQS_CLIENT.send_message(QueueUrl=SQS_RESPONSE_QUEUE_URL, MessageBody="error", MessageGroupId="TA", MessageAttributes={
+                'email': {
+                    'StringValue': email,
+                    'DataType': 'String'
+                },
+                'requested_tickets': {
+                    'StringValue': requested_tickets,
+                    'DataType': 'String'
+                },
+                'requested_event': {
+                    'StringValue': requested_event,
+                    'DataType': 'String'
+                },
+                'transaction_id': {
+                    'StringValue': transaction_id,
+                    'DataType': 'String'
+                },
+                'error': {
+                    'StringValue': str(e),
+                    'DataType': 'String'
+                }
+            })
